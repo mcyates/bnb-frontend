86f874f9b37dbfcae976cb1c66053775f3198311
Matthew Yates <33201611+M-C-Yates@users.noreply.github.com>
2019-05-24 18:58:45 -0400

    Dev (#19)
    * .
    
    * you can delete listings now
    
    * ui now properly updates when state changes
    
    * refactored front end and listing components
    
    * refactored front page further
    
    * .
    
    * you can now book a listing but ui does not accurately reflect this or the total price if a month rolls over
    
    * now displays when a listing is booked + total trip price
    
    * listings can now have multiple bookings

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -8,6 +8,7 @@ import { LISTINGS } from "../../queries/LISTINGS";
 
 import { Link } from "@reach/router";
 import { Booking } from "../Booking/Booking";
+import { MY_LISTINGS } from "../../queries/MY_LISTINGS";
 
 export const Listing = (props) => {
 	return (
@@ -54,6 +55,8 @@ export const Listing = (props) => {
 												{
 													query: LISTINGS,
 													variables: { id: props.id }
+												}, {
+													query: MY_LISTINGS
 												}
 											]}
 										>
@@ -75,7 +78,7 @@ export const Listing = (props) => {
 										</>
 										) : (
 											<>
-											<Booking id={props.id} price={listing.price} booking={listing.booking} />
+											<Booking id={props.id} price={listing.price} bookings={listing.bookings} />
 											<h3>{listing.name}</h3>
 											<p>{listing.category}</p>
 											<img src={listing.heroUrl} alt="hero" />
