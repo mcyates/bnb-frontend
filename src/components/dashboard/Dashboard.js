f1ef958ee231dbdb7c9ebf0c922999833918e325
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-05-27 14:43:14 -0400

    implemented infinite scroll pagination for the front page and dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-import { Link } from "@reach/router";
 import { Query } from "react-apollo";
-import { MY_LISTINGS } from '../../queries/MY_LISTINGS';
+import { MY_LISTINGS } from "../../queries/MY_LISTINGS";
 import { GET_USER } from "../../queries/GET_USER";
+import { ListingList } from "../listingList/ListingList";
+import { Link } from "@reach/router";
 
 export const Dashboard = () => {
 	return (
@@ -17,35 +18,48 @@ export const Dashboard = () => {
 						return `Error: ${error}`;
 					}
 					return (
-						<Query query={MY_LISTINGS} pollInterval={5000}>
-							{({ loading, error, data }) => {
+						<Query
+							query={MY_LISTINGS}
+							variables={{
+								first: 10,
+								skip: 0
+							}}
+						>
+							{({ loading, error, data, fetchMore }) => {
 								if (loading) {
 									return null;
 								}
 								if (error) {
 									return `Error: ${error}`;
 								}
+								const listings = data.mylistings;
+								let cursor = listings[listings.length - 1].id;
 								return (
-									<>
-										{data.mylistings.map((listing) => {
-											const url = `/listing/${listing.id}`;
-											return (
-												<Link key={listing.id} to={url}>
-													<div>
-														{listing.heroUrl ? (
-															<img
-																src={listing.heroUrl}
-																alt={`Listing Hero ${listing.id}`}
-															/>
-														) : (
-															<></>
-														)}
-														<h3>{listing.name}</h3>
-													</div>
-												</Link>
-											);
-										})}
-									</>
+									<React.Fragment>
+										<ListingList
+											entries={listings}
+											onLoadMore={() => {
+												fetchMore({
+													variables: {
+														after: cursor
+													},
+													updateQuery: (prevResult, { fetchMoreResult }) => {
+														let newListings = fetchMoreResult.mylistings;
+
+														if (newListings.length) {
+															cursor = newListings[newListings.length - 1].id;
+														}
+														return Object.assign({}, prevResult, {
+															mylistings: [
+																...prevResult.mylistings,
+																...newListings
+															]
+														});
+													}
+												});
+											}}
+										/>
+									</React.Fragment>
 								);
 							}}
 						</Query>
