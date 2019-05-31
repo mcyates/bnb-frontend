d230937177fbe92722e7a273fbd5ab3a3419fa40
Matthew Yates <33201611+M-C-Yates@users.noreply.github.com>
2019-05-31 16:42:21 -0400

    Dev (#22)
    * started to style listing list
    
    * further styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,34 +6,35 @@ import Logout from "../logout/logout";
 import { Query } from "react-apollo";
 import { GET_AUTHED } from "../../queries/GET_AUTHED";
 
-
-
 const Navbar = () => {
 	return (
-    <Query query={GET_AUTHED} >
-    {({data: {isAuthed}}) => (
-      <nav className="Navbar">
-      <Link className="link--logo" to="/">
-        <img className="Logo" src={airbnbsvg} alt="airbnb-logo" />
-      </Link>
-      <div className="rightCorner">
-        {
-          isAuthed ? (
-            <>
-            <Link className="nav-link" to="/dashboard">Dashboard</Link>
-            <Logout />
-            </>
-          ) : (
-            <>
-            <Link className="nav-link" to="/register">Sign up</Link>
-            <Link className="nav-link" to="login">Log in</Link>
-            </>
-          )
-        }
-          </div>
-      </nav>
-    )}
-
+		<Query query={GET_AUTHED}>
+			{({ data: { isAuthed } }) => (
+				<nav className="Navbar">
+					<Link className="link--logo" to="/">
+						<img className="Logo" src={airbnbsvg} alt="airbnb-logo" />
+					</Link>
+					<div className="rightCorner">
+						{isAuthed ? (
+							<>
+								<Link className="nav-link" to="/dashboard">
+									Dashboard
+								</Link>
+								<Logout />
+							</>
+						) : (
+							<>
+								<Link className="nav-link" to="/register">
+									Sign up
+								</Link>
+								<Link className="nav-link" to="login">
+									Log in
+								</Link>
+							</>
+						)}
+					</div>
+				</nav>
+			)}
 		</Query>
 	);
 };
